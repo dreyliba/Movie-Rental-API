74c1166107c3fd028a6a4326f482c5bc54363dab
Drey Liba <libaandrey@gmail.com>
2020-11-16 19:07:38 +0800

    Success

--- src/index.js
+++ src/index.js
@@ -1,23 +1,24 @@
 import express from 'express';
-import mongoose from 'mongoose';
+import mongoose, { model, Schema } from 'mongoose';
 
 const app = express();
 app.use(express.json());
 
+const genreSchema = new mongoose.Schema({
+    name: { type: String, require: true },
+    description: { type: String, require: true },
+});
 
-const movieSchema = new mongoose.Schema({
+const Genre = mongoose.model('Genre', genreSchema);
+
+const Movie = mongoose.model('Movie', {
     title: { type: String, require: true },
     description: { type: String, require: true },
     copies: { type: Number, require: true },
     IsAvailable: { type: Boolean, default: true},
+    genre: { type: genreSchema, require: true },
 });
-const Movie = mongoose.model('Movie', movieSchema);
 
-const Genre = mongoose.model('Genre', {
-    name: { type: String, require: true },
-    description: { type: String, require: true },
-    genre: { type: movieSchema, require: true },
-});
 
     app.get('/api/genries', async (req, res) => {
         const genries = await Genre.find();
@@ -61,11 +62,11 @@ const Genre = mongoose.model('Genre', {
         const movies = await Movie.findById(req.params.id);
         res.send(movies);
     });
-
+    
     app.post('/api/movies', async (req, res) => {
-        const { title, description, copies, IsAvailable, genreID } = req.body;
+        const { title, description, copies, genreID, IsAvailable } = req.body;
         const genre = await Genre.findById(genreID);
-        const movies = new Movie ({
+        const movies = new Movie({
             title,
             description,
             copies,
@@ -74,8 +75,8 @@ const Genre = mongoose.model('Genre', {
                 _id: genre._id,
                 name: genre.name,
             },
-        });
-        const result = await movies.save();	
+        });   
+        const result = await movies.save();
         res.send(result);
     });
 
@@ -84,13 +85,13 @@ const Genre = mongoose.model('Genre', {
             title: req.body.title,
             description: req.body.description,
             copies: req.body.copies,
-            // IsAvailable: req.body.IsAvailable,
+            IsAvailable: req.body.IsAvailable,
         };
         const movies = await Movie.findByIdAndUpdate(req.params.id, body, {
             new: true,
         });
         res.send(movies);
-    });
+    }); 
 
     app.delete('/api/movies/:id', async (req, res) => {
         const result = await Movie.findByIdAndDelete(req.params.id);
